3682f9b254a356cfb8547cd98229118083c4edaf
DKATZZZ <92221627+DKATZZZ@users.noreply.github.com>
2023-05-11 00:13:31 +0530

    commit changes to master

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import StudentData from './studentdata';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentData />
+    </div>
+  );
+}
+
+export default App;
